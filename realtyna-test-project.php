b2e6dbfc8b127661ec787fd3abadcf146fe23bc5
Amir Sasani <sasani.amir.1998@gmail.com>
2022-11-26 09:14:15 +0330

    add plugins main file

---
+++ realtyna-test-project.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Realtyna Test
+ * Plugin URI:        https://github.com/amirsasani/realtyna-test-project
+ * Description:       A test project from RealTyna. Wordpress plugin
+ * Version:           1.0.0
+ * Author:            Amir Sasani
+ * Author URI:        https://amirsasani.ir
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       realtyna-test
+ * Domain Path:       /languages
+ *
+ * @package PowerBlocks
+ */
+
+namespace AmirSasani\RealtynaTest;
