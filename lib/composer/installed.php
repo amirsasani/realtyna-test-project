be24942cffcddeaf207cc3ed8993858ce8652768
Amir Sasani <sasani.amir.1998@gmail.com>
2022-11-28 15:02:11 +0330

    update .gitignore and add lib directory to git

---
+++ lib/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'amir/realtyna-test-project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5dc4b611a5a52e92213623ce28977c6d799f2ff4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'amir/realtyna-test-project' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5dc4b611a5a52e92213623ce28977c6d799f2ff4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
